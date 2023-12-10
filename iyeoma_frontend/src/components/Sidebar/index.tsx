a47e88df815e6ae04269f17d646a83e691fc2a92
Solexi <sophia.ezeh@pau.edu.ng>
2023-12-10 22:11:18 +0100

    Initial commit

---
+++ iyeoma_frontend/src/components/Sidebar/index.tsx
@@ -0,0 +1,214 @@
+import { Flex, Link as ChakraLink, Box, Icon } from "@chakra-ui/react";
+import { IoPerson,} from "react-icons/io5";
+import { SlSettings } from "react-icons/sl";
+// import Logo from "../../images/Logo.png";
+import "typeface-karla";
+import { CSSProperties, useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+// import dashboard from "../../images/dashboard.svg";
+// import activity from "../../images/activity.svg";
+import { FaBook, } from "react-icons/fa6";
+
+const Sidebar = () => {
+    const { linkStyle, _activeLink } = useStyles();
+    const location = useLocation();
+    const [activeLink, setActiveLink] = useState<string | null>(null);
+
+    useEffect(() => {
+        setActiveLink(location.pathname);
+    }, [location.pathname]);
+
+    return (
+        <Flex
+            flexDir={"column"}
+            // justify={"space-between"}
+            // pos={"fixed"}
+            boxShadow={"md"}
+            align={"left"}
+            h={"100vh"}
+            w={"250px"}
+            bg={"#390048"}
+            py={"70px"}
+        >
+            <ChakraLink
+                href="/"
+                _hover={{ textDecoration: "none" }}
+                textDecoration={"none"}
+                mb={"100px"}
+                mx={"20px"}
+            >
+                <Box
+                    display={"flex"}
+                    flexDir={"row"}
+                >
+                    {/* <Image
+                        src={`url(https://avatars.githubusercontent.com/u/82169299?s=96&v=4)`}
+                        w={"40px"}
+                        h={"40px"}
+                        mr={"12px"}
+                    /> */}
+                    <Box
+                        color={"#eeeeee"}
+                        fontSize="26px"
+                        fontWeight={700}
+                    >
+                        Iyeoma
+                    </Box>
+                </Box>
+            </ChakraLink>
+            <Flex
+                mb={"200px"}
+                flexDir={"column"}
+                gap={"10px"}
+            >
+                <ChakraLink
+                    as={Link}
+                    display={"flex"}
+                    flexDir={"row"}
+                    justifyItems={"left"}
+                    alignItems={"center"}
+                    to={"/dashboard"}
+                    px={"20px"}
+                    _hover={{ borderInline: "5px solid #050F28", boxShadow: "md", transition: "0.2s ease-in-out" }}
+                    style={activeLink === "/profile" ? _activeLink : linkStyle}
+                >
+                        <Icon
+                            as={IoPerson}
+                            mr={"20px"}
+                            w={"18px"}
+                            h={"18px"}
+
+                        />
+                        Profile
+                </ChakraLink>
+                <ChakraLink
+                    as={Link}
+                    display={"flex"}
+                    flexDir={"row"}
+                    justifyItems={"left"}
+                    alignItems={"center"}
+                    to={"/dashboard"}
+                    px={"20px"}
+                    _hover={{ borderInline: "5px solid #050F28", boxShadow: "md", transition: "0.2s ease-in-out" }}
+                    style={activeLink === "/dashboard" ? _activeLink : linkStyle}
+                >
+                        {/* <Icon
+                            as={Image}
+                            src={dashboard}
+                            mr={"20px"}
+                            w={"18px"}
+                            h={"18px"}
+                        /> */}
+                        Dashboard
+                </ChakraLink>
+                <ChakraLink
+                    as={Link}
+                    display={"flex"}
+                    flexDir={"row"}
+                    justifyItems={"left"}
+                    alignItems={"center"}
+                    to={"/activity"}
+                    pl={"20px"}
+                    _hover={{ borderInline: "5px solid #050F28", boxShadow: "md", transition: "0.2s ease-in-out" }}
+                    style={activeLink === "/activity" ? _activeLink : linkStyle}
+                >
+                        {/* <Icon
+                            as={Image}
+                            src={activity}
+                            mr={"20px"}
+                            w={"20px"}
+                            h={"20px"}
+                        /> */}
+                        Activity
+                </ChakraLink>
+                <ChakraLink
+                    as={Link}
+                    w={"100%"}
+                    display={"flex"}
+                    flexDir={"row"}
+                    justifyItems={"left"}
+                    alignItems={"center"}
+                    to={"/afrilearn"}
+                    pl={"20px"}
+                    _hover={{ borderInline: "5px solid #050F28", boxShadow: "md", transition: "0.2s ease-in-out" }}
+                    style={activeLink === "/afrilearn" ? _activeLink : linkStyle}
+                >
+                        <Icon
+                            as={FaBook}
+                            mr={"20px"}
+                            w={"20px"}
+                            h={"20px"}
+                        />
+                        Afrilearn
+                </ChakraLink>
+            </Flex>
+            <Flex
+                flexDir={"column"}
+                gap={"10px"}
+            >
+                <ChakraLink
+                    as={Link}
+                    display={"flex"}
+                    flexDir={"row"}
+                    justifyItems={"left"}
+                    alignItems={"center"}
+                    to={"/dashboard"}
+                    px={"20px"}
+                    _hover={{ borderInline: "5px solid #050F28", boxShadow: "md", transition: "0.2s ease-in-out" }}
+                    style={activeLink === "/settings" ? _activeLink : linkStyle}
+                >
+                        <Icon
+                            as={SlSettings}
+                            w={"18px"}
+                            h={"18px"}
+                            mr={"20px"}
+                        />
+                        Settings
+                </ChakraLink>
+                {/* <ChakraLink
+                    as={Link}
+                    href={"/info"}
+                    _hover={{ borderInline: "8px solid #050F28", boxShadow: "md", transition: "0.2s ease-in-out" }}
+                    style={activeLink === "/info" ? _activeLink : linkStyle}
+                >
+                    <Box
+                        w={"214px"}
+                        justifyContent={"center"}
+                    >
+                        <Icon
+                            as={IoInformationCircleOutline}
+                            w={"25px"}
+                            h={"25px"}
+                            mr={"20px"}
+                        />
+                        Info
+                    </Box>
+                </ChakraLink> */}
+            </Flex>
+        </Flex>
+    )
+}
+
+export default Sidebar;
+const useStyles = () => {
+    return {
+        linkStyle: {
+            color: "#ecf",
+            fontSize: "20px",
+            fontWeight: 500,
+            fontFamily: "Karla",
+            lineHeight: "50px",
+            textAlign: "center",
+        } as CSSProperties,
+        _activeLink: {
+            color: "#ecf",
+            fontSize: "20px",
+            fontWeight: 500,
+            boxShadow: "md",
+            fontFamily: "Karla",
+            lineHeight: "50px",
+            textAlign: "center",
+            borderInline: "5px solid #050F28",
+        } as CSSProperties,
+    };
+};
---
+++ iyeoma_frontend/src/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,14 @@
+import { Flex } from "@chakra-ui/react";
+import Sidebar from "../../components/Sidebar";
+
+const Dashboard = () => {
+    return (
+        <Flex
+            bg={"#f2f6ff"}
+        >
+            <Sidebar/>
+        </Flex>
+    )
+}
+
+export default Dashboard;
